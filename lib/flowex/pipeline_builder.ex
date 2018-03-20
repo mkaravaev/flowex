0073a1ff63b33e8e8d6412ed3e22c574a00a823f
Mikhail Karavaev <mkaravaev@gmail.com>
2018-03-20 21:44:48 +0500

    Update pipeline_builder.ex

--- lib/flowex/pipeline_builder.ex
+++ lib/flowex/pipeline_builder.ex
@@ -13,13 +13,12 @@ defmodule Flowex.PipelineBuilder do
   end
 
   def supervised_start(pipeline_module, pid, opts) do
-    IO.inspect("Testing super!")
+    IO.inspect("Testing!")
     {producer_name, consumer_name, all_specs} = build_children(pipeline_module, opts)
 
     sup_name = supervisor_name(pipeline_module)
     sup_spec = supervisor(Flowex.Supervisor, [all_specs, sup_name], [id: sup_name, restart: :permanent])
-    IO.inspect(sup_name)
-    IO.inspect(sup_spec)
+
     Supervisor.start_child(pid, sup_spec) |> IO.inspect
     pipeline_struct(pipeline_module, producer_name, consumer_name, sup_name)
   end
